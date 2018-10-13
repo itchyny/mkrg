b2d36b5483e1a2ac562385e2b4864b4864c217cf
itchyny <itchyny@hatena.ne.jp>
2018-10-13 23:06:14 +0900

    start implementing multiple graphs

--- app.go
+++ app.go
@@ -2,6 +2,7 @@ package mkrg
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/mackerelio/mackerel-client-go"
@@ -22,18 +23,30 @@ func NewApp(client *mackerel.Client, hostID string) *app {
 func (app *app) Run() error {
 	now := time.Now().Round(time.Minute)
 	from := now.Add(-3 * time.Hour)
-	metricNames := []string{"loadavg1", "loadavg5", "loadavg15"}
-	ms := make(metricsByName, len(metricNames))
-	for _, metricName := range metricNames {
-		metrics, err := app.client.FetchHostMetricValues(app.hostID, metricName, from.Unix(), now.Unix())
-		if err != nil {
-			return err
+	allMetricNames := []string{"loadavg1", "loadavg5", "loadavg15", "cpu.user.percentage", "cpu.system.percentage"}
+	graphNames := []string{"loadavg", "cpu"}
+	for _, graphName := range graphNames {
+		var metricNames []string
+		for _, metricName := range allMetricNames {
+			if strings.HasPrefix(metricName, graphName) {
+				metricNames = append(metricNames, metricName)
+			}
 		}
-		ms.Add(metricName, metrics)
-	}
-	v := newViewer("loadavg", 100, 180)
-	for _, l := range v.GetLines(ms, from) {
-		fmt.Println(l)
+		if len(metricNames) == 0 {
+			continue
+		}
+		ms := make(metricsByName, len(metricNames))
+		for _, metricName := range metricNames {
+			metrics, err := app.client.FetchHostMetricValues(app.hostID, metricName, from.Unix(), now.Unix())
+			if err != nil {
+				return err
+			}
+			ms.Add(metricName, metrics)
+		}
+		v := newViewer(graphName, 100, 180)
+		for _, l := range v.GetLines(ms, from) {
+			fmt.Println(l)
+		}
 	}
 	return nil
 }

21b55728cc977f920f06b340a06dcad409828cfe
itchyny <itchyny@hatena.ne.jp>
2018-10-13 23:30:03 +0900

    fetch only posted metrics

--- app.go
+++ app.go
@@ -20,12 +20,18 @@ func NewApp(client *mackerel.Client, hostID string) *app {
 }
 
 func (app *app) Run() error {
+	metricNamesMap, err := app.getMetricNamesMap()
+	if err != nil {
+		return err
+	}
 	now := time.Now().Round(time.Minute)
 	from := now.Add(-3 * time.Hour)
 	for _, graph := range systemGraphs {
 		var metricNames []string
 		for _, metric := range graph.metrics {
-			metricNames = append(metricNames, metric.name)
+			if metricNamesMap[metric.name] {
+				metricNames = append(metricNames, metric.name)
+			}
 		}
 		if len(metricNames) == 0 {
 			continue
@@ -44,4 +50,16 @@ func (app *app) Run() error {
 		}
 	}
 	return nil
+}
+
+func (app *app) getMetricNamesMap() (map[string]bool, error) {
+	metricNames, err := app.client.ListHostMetricNames(app.hostID)
+	if err != nil {
+		return nil, err
+	}
+	metricNamesMap := make(map[string]bool, len(metricNames))
+	for _, metricName := range metricNames {
+		metricNamesMap[metricName] = true
+	}
+	return metricNamesMap, nil
 }

74c980f362d989b4176a27d1cbacf4997f43c50c
itchyny <itchyny@hatena.ne.jp>
2018-10-14 18:21:20 +0900

    specify width of iTerm2 viewer against the session width

--- iterm2.go
+++ iterm2.go
@@ -9,17 +9,18 @@ import (
 
 type iterm2 struct {
 	height, width int
+	maxColumn     int
 	from, until   time.Time
 }
 
-func newIterm2(height, width int, from, until time.Time) *iterm2 {
-	return &iterm2{height, width, from, until}
+func newIterm2(height, width int, maxColumn int, from, until time.Time) *iterm2 {
+	return &iterm2{height, width, maxColumn, from, until}
 }
 
 func (ui *iterm2) output(graph graph, ms metricsByName) error {
 	buf := new(bytes.Buffer)
 	printImage(buf, graph, ms, ui.height*24, ui.width*16, ui.from, ui.until)
-	fmt.Print("\x1b]1337;File=inline=1;preserveAspectRatio=1:")
+	fmt.Printf("\x1b]1337;File=inline=1;preserveAspectRatio=1;width=%d%%:", 100.0/ui.maxColumn)
 	fmt.Print(base64.StdEncoding.EncodeToString(buf.Bytes()))
 	fmt.Print("\x07\n")
 	return nil

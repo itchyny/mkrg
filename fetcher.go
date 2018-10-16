8b649df730217e01175e81f2d4c6d381240e584f
itchyny <itchyny@hatena.ne.jp>
2018-10-16 23:38:20 +0900

    rename metricApiResult to metricAPIResult

--- fetcher.go
+++ fetcher.go
@@ -11,7 +11,7 @@ type fetcher struct {
 	sem    chan struct{}
 }
 
-type metricApiResult struct {
+type metricAPIResult struct {
 	metricName string
 	metrics    []mackerel.MetricValue
 	err        error
@@ -21,13 +21,13 @@ func newFetcher(client *mackerel.Client) *fetcher {
 	return &fetcher{client, make(chan struct{}, 5)}
 }
 
-func (f *fetcher) fetchMetric(hostID, metricName string, from, until time.Time) <-chan metricApiResult {
-	ch := make(chan metricApiResult)
+func (f *fetcher) fetchMetric(hostID, metricName string, from, until time.Time) <-chan metricAPIResult {
+	ch := make(chan metricAPIResult)
 	f.sem <- struct{}{}
 	go func() {
 		metrics, err := f.client.FetchHostMetricValues(hostID, metricName, from.Unix(), until.Unix())
 		<-f.sem
-		ch <- metricApiResult{metricName, metrics, err}
+		ch <- metricAPIResult{metricName, metrics, err}
 	}()
 	return ch
 }
